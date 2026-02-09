9ff0df7d448f8587f4bcac222a25cdcc568f5e0a
Kitsune <91675305+Agakitsune@users.noreply.github.com>
2026-02-09 18:30:20 +0000

    Revert "Add new Menu and Popup System"

--- src/window/window.cpp
+++
@@ -1,9 +0,0 @@
-#include "window/window.hpp"
-
-namespace chroma {
-
-    Window::Window(const std::string &label, ImGuiWindowFlags flags) noexcept
-    : label(label), flags(flags)
-    {
-    }
-}
